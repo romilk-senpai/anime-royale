81050041cd2e5ca1f7abc5d47089615dbe48e91e
romilk-senpai <rpakhomov1702@gmail.com>
2024-11-12 17:09:13 +0200

    fixed coord transformation functions

--- src/core/camera.c
+++ src/core/camera.c
@@ -11,12 +11,13 @@ Camera *camera_new(Vector2 screen_size) {
 }
 
 Vector2 screen_to_world_pos(Camera *camera, Vector2 screen_pos) {
-  return vector2_sub(
-      screen_pos,
-      vector2_add(camera->position, vector2_mul_scalar(camera->viewbox, 0.5f)));
+  Vector2 relative_screen_pos =
+      vector2_sub(screen_pos, vector2_mul_scalar(camera->viewbox, 0.5f));
+  return vector2_add(relative_screen_pos, camera->position);
 }
 
 Vector2 world_to_screen_pos(Camera *camera, Vector2 world_pos) {
-  return vector2_add(vector2_sub(world_pos, camera->position),
+  Vector2 relative_world_pos = vector2_sub(world_pos, camera->position);
+  return vector2_add(relative_world_pos,
                      vector2_mul_scalar(camera->viewbox, 0.5f));
 }
