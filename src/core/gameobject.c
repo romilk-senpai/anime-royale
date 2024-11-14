f1afe7342dc2410ff3677aa4869cf8fb2f03253f
romilk-senpai <rpakhomov1702@gmail.com>
2024-11-15 01:54:58 +0200

    init z index to 0

--- src/core/gameobject.c
+++ src/core/gameobject.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 GameObject *go_create(uint32_t instance_id, void *binding, update_func update,
-                   render_func render) {
+                      render_func render) {
   GameObject *go = malloc(sizeof(GameObject));
   *go = (GameObject){.instance_id = instance_id,
                      .binding = binding,
                      .position = vector2_zero(),
                      .angle = 0.0,
                      .update = update,
-                     .render = render};
+                     .render = render,
+                     .z_index = 0};
   return go;
 }
