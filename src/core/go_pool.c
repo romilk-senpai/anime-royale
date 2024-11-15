be1f5229dd9d5be57df547a3ea8e8c8155d5184c
romilk-senpai <rpakhomov1702@gmail.com>
2024-11-15 05:16:43 +0200

    unbund impl :]

--- src/core/go_pool.c
+++ src/core/go_pool.c
@@ -35,7 +35,7 @@ GOPool *go_pool_new() {
 uint32_t go_pool_new_id(GOPool *go_pool) { return go_pool->next_id++; }
 
 void go_pool_bind(GOPool *go_pool, GameObject *go) {
-  if (hash_map_contains(go_pool->go_map, &go->instance_id) == 1) {
+  if (hash_map_contains(go_pool->go_map, &go->instance_id)) {
     fprintf(stderr, "pool already contains an object with id %u: %u\n",
             go->instance_id,
             ((GameObject *)hash_map_get(go_pool->go_map, &go->instance_id))
@@ -45,4 +45,13 @@ void go_pool_bind(GOPool *go_pool, GameObject *go) {
   hash_map_set(go_pool->go_map, &go->instance_id, go);
 }
 
-void go_pool_unbind(GOPool *go_pool, GameObject *go) {}
+void go_pool_unbind(GOPool *go_pool, GameObject *go) {
+    if (!hash_map_contains(go_pool->go_map, &go->instance_id)) {
+    fprintf(stderr, "pool does not contain an object with id %u: %u\n",
+            go->instance_id,
+            ((GameObject *)hash_map_get(go_pool->go_map, &go->instance_id))
+                ->instance_id);
+    return;
+  }
+  hash_map_delete(go_pool->go_map, &go->instance_id);
+}
