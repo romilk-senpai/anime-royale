bb053aa2e088fb3a40cbd05b121c2b78e67239fb
romilk-senpai <rpakhomov1702@gmail.com>
2024-11-23 04:42:51 +0200

    separated hash funcs

--- src/core/go_pool.c
+++ src/core/go_pool.c
@@ -3,27 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-static uint32_t djb2_hash(void *val) {
-  uint32_t value = *(uint32_t *)val;
-  uint32_t hash = 5381;
-  for (int i = 0; i < 4; i++) {
-    uint8_t byte = (value >> (i * 8)) & 0xFF;
-    hash = ((hash << 5) + hash) + byte;
-  }
-  return hash;
-}
-
-static uint32_t fnv1a_hash(void *val) {
-  uint32_t value = *(uint32_t *)val;
-  uint32_t hash = 2166136261u;
-  for (int i = 0; i < 8; i++) {
-    uint8_t byte = (value >> (i * 8)) & 0xFF;
-    hash ^= byte;
-    hash *= 16777619u;
-  }
-  return hash;
-}
+#include <hash.h>
 
 GOPool *go_pool_new() {
   GOPool *go_manager = malloc(sizeof(GOPool));
---
+++ src/core/hash.h
@@ -0,0 +1,26 @@
+#ifndef HASH_FUNCS_H
+#define HASH_FUNCS_H
+
+#include <stdint.h>
+
+static uint32_t djb2_hash(void *val) {
+  uint32_t value = *(uint32_t *)val;
+  uint32_t hash = 5381;
+  for (int i = 0; i < 4; i++) {
+    uint8_t byte = (value >> (i * 8)) & 0xFF;
+    hash = ((hash << 5) + hash) + byte;
+  }
+  return hash;
+}
+
+static uint32_t fnv1a_hash(void *val) {
+  uint32_t value = *(uint32_t *)val;
+  uint32_t hash = 2166136261u;
+  for (int i = 0; i < 8; i++) {
+    uint8_t byte = (value >> (i * 8)) & 0xFF;
+    hash ^= byte;
+    hash *= 16777619u;
+  }
+  return hash;
+}
+#endif // !HASH_FUNCS_H
